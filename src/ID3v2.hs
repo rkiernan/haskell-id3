18a9bea46e5c37f6793aaaf4fb9afef34eceedb6
Nick Condron <ncondron42@gmail.com>
2017-11-05 14:18:12 -0500

    id3v2 parser outline

--- src/ID3v2.hs
+++ src/ID3v2.hs
@@ -1,4 +1,10 @@
-import Prelude hiding (take)
+{-# LANGUAGE OverloadedStrings #-}
+
+module ID3v2
+    ( ID3v2
+    , id3v2
+    ) where
+
 import Data.Word (Word8)
 import Data.Maybe (maybe)
 import qualified Data.ByteString.Char8 as C
@@ -6,11 +12,12 @@ import qualified Data.ByteString as B
 import Data.Attoparsec.ByteString
 import Data.Bits
 
-data ID3v2 = ID3v2
-    { header
-    , extHeader ::
-    }
+import Tag
 
+data ID3v2 = ID3v2
+    { tagHeader :: Header
+    , tagExtHeader :: Maybe ExtHeader
+    }
 
 data Header = Header
       {
@@ -21,6 +28,12 @@ data Header = Header
       experimental :: Bool,
       size :: Int
       }
+
+data ExtHeader = ExtHeader
+    { extSize :: Int
+    , flags :: Bool
+    , padding :: Int
+    }
 
 header :: Parser Header
 header = do
@@ -34,3 +47,14 @@ header = do
   let experimental = testBit flags 2
 
   return $ Header majorVersion minorVersion unsync extended experimental sizeResult
+
+id3v2 :: Parser ID3v2
+id3v2 = do
+    h <- header
+    e <- if extended h then extHeader >>= Just else return Nothing
+    return $ ID3v2 h e
+
+extHeader :: Parser ExtHeader
+extHeader = do
+    return ExtHeader 0 false 0
+
